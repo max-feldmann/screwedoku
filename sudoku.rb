8b2c5c5cb27c61d6f89da8a33c5b8048d3be14fb
Max Feldmann <maxfeldmann.consultant@gmail.com>
2020-04-14 08:37:14 +0200

    Test

--- sudoku.rb
+++ sudoku.rb
@@ -10,17 +10,16 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
-  end
+def parse_val(val)
+  Integer(val)
+end
+
+def parse_pos(pos)
+  pos.split(",")
+end
 
   def get_pos
     pos = nil
